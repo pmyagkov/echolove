b6f26c593b5d59e526dde7cfbabff18752f8f5e7
Pavel Myagkov <p.myagkov@corp.mail.ru>
2016-05-20 18:50:08 +0300

    Base server

---
+++ server/index.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var WebSocket = require('ws');
+var _ = require('lodash');
+
+// подключенные клиенты
+var clients = {};
+var clientIndex = 0;
+
+var SOCKET_PORT = 8070;
+var APP_PORT = 8069;
+
+var wss = new WebSocket.Server({
+  port: SOCKET_PORT
+});
+
+wss.on('connection', function(ws) {
+
+  var id = clientIndex++;
+  clients[id] = ws;
+  console.log('New connection', id);
+
+  ws.on('message', function(message) {
+    console.log('Incoming message from client', id, ' | ', message);
+  });
+
+  ws.on('close', function() {
+    console.log('Connection closed', id);
+    delete clients[id];
+  });
+
+});
+
+
+function sendMessageToClient(id, message) {
+  var cls;
+  if (!id || !clients[id]) {
+    cls = _.values(clients);
+  } else {
+    cls = [clients[id]];
+  }
+
+  cls.forEach(function(client) {
+    client.send(message);
+  });
+}
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/play', function (req, res) {
+  var url = req.body.url;
+
+  if (!url) {
+    return res.status(400).send('No `url` passed');
+  }
+
+  sendMessageToClient(null, { url: url });
+
+  /*
+  var name = req.body.name;
+  if (!name || name === '@') {
+    name = 'Anonymous';
+  }
+
+  var message = text + '\n\n' + name;
+  var url = 'https://api.telegram.org/bot190843896:AAFchCFzLnhq-H9FG0wZABviItMBA3_HCuo/sendMessage';
+  var data = {chat_id: '@JIRAbusFeedback', text: message};
+
+  request.post({ url: url, form: data }, function (err, httpResponse, body) {
+    console.log('TELEGRAM RESPONSE', body);
+
+    try {
+      body = JSON.parse(body);
+    } catch (e) {}
+
+    res.set('Access-Control-Allow-Origin', '*');
+
+    var text;
+    if (err || !body.ok) {
+      text = 'Request error: ' + JSON.stringify(err) + '\n\n' + 'Response payload: ' + JSON.stringify(body);
+      res.status(502).send(text);
+    } else {
+      res.status(200).end('Success!');
+    }
+
+  });*/
+});
+
+app.listen(APP_PORT, function () {
+  console.log('Smoothy server app listening on port 8069!');
+});
